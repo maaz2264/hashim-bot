48d377ede6c36e7e64a883d5f03c32625d2f0b55
aicreations1 <163501436+aicreations1@users.noreply.github.com>
2024-03-17 22:14:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = '𝙷𝚊𝚜𝚑𝚒𝚖';
+global.owner = ['917034794304'];
+global.premium = ['919747809055'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
+global.author = '𝙷𝚊𝚜𝚑𝚒𝚖';
 global.sessionName = 'session';
 global.prefa = ['', '!', '.'];
 global.sp = '✪';
