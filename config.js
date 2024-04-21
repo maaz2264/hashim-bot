31ed9730b5f1d177ddb0bf59a71e65d06f276ea1
maaz2264 <143191256+maaz2264@users.noreply.github.com>
2024-04-21 12:24:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
+global.link = 'https://whatsapp.com/channel/0029VaVoMBE05MUaK5Gbwk2p'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.owner = ['923192106398'];
+global.premium = ['1(786)7910846'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
